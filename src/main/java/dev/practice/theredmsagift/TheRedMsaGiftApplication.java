a072f2a67a3aa065684c509e1a5086c69a393a40
JayPark7821 <pjhyun1990@gmail.com>
2022-11-07 22:29:35 +0900

    initial commit

---
+++ src/main/java/dev/practice/theredmsagift/TheRedMsaGiftApplication.java
@@ -0,0 +1,13 @@
+package dev.practice.theredmsagift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRedMsaGiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRedMsaGiftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/practice/theredmsagift/TheRedMsaGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.practice.theredmsagift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRedMsaGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
